c2a80dc64a2e95e02c5e4aac248b424dd14cb0da
Sathish g <srisrisrisathish176@gmail.com>
2025-10-14 13:04:14 +0530

    removed connectivity plus

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,18 +1,13 @@
 import UIKit
 import Flutter
 
-@main
-class AppDelegate: FlutterAppDelegate {
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    
-    // Add safe plugin registration with error handling
-    DispatchQueue.main.async {
-        GeneratedPluginRegistrant.register(with: self)
-    }
-    
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
